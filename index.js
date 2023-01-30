583b91e81c39d705cd70610f116a74b2509bb20c
HernandoEscobar <122547595+HernandoEscobar@users.noreply.github.com>
2023-01-30 10:44:13 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+function cambiarCont() {
+    areaCont.innerText = "  010101011010  felixwalters@example.com  www.felixwalters.com"
+}
+function volverCont() {
+    areaCont.innerText = "Contacto" 
+}
+
+function cambiarSobre() {
+    areaSobre.innerText = "Cuento con habilidades de cara a la comprensión, al trabajo en equipo y al aprendizaje rápido."
+}
+function volverSobre() {
+    areaSobre.innerText = "Sobre mi"
+}
+
+function cambiarInt() {
+    areaInt.innerText = "Practicante de yoga. La practica de esta disciplina desde hace varios años me ha ayudado de complemento vital para el desarrollo de equilibrio y la serenidad interior."
+}
+
+function volverInt() {
+    areaInt.innerText = "Intereses"
+}
